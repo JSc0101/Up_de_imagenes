dacf36493bcb0057332cdb9530fd8a365afe5093
Sebastian009w <ia3266505@gmail.com>
2023-03-01 21:54:45 -0500

    se utiliza el middlewares morgan y se configura la carpeta public --

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import express from "express";
+import morgan from "morgan";
+import routes from "./routes/index.routes";
+import path from "path";
 
 const app = express();
 
 // # Setting
-
 app.set("port", process.env.PORT || 3000);
+
+// # middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/api", routes);
+
+// # public
+app.use("/public", express.static(path.resolve("public")));
+
 export default app;
